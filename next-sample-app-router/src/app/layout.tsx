3572c09e6f93f1823ee3541d22a5d56ae818caaf
Timofey Kachalov <timofei@metabase.com>
2025-05-16 19:36:55 +0400

    Add e2e workflow and respect the WATCH env var when running in Docker

--- next-sample-app-router/src/app/layout.tsx
+++ next-sample-app-router/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import {
   defineMetabaseAuthConfig,
   MetabaseProvider,
+  defineMetabaseTheme,
 } from "@metabase/embedding-sdk-react/nextjs";
 import type { Metadata } from "next";
 
@@ -33,6 +34,50 @@ const authConfig = defineMetabaseAuthConfig({
   authProviderUri: `/api/metabase/auth`,
 });
 
+const theme = defineMetabaseTheme({
+  // Specify a font to use from the set of fonts supported by Metabase.
+  // You can set the font to "Custom" to use the custom font
+  // configured in your Metabase instance.
+  fontFamily: "Lato",
+
+  // Override the base font size for every component.
+  // This does not usually need to be set, as the components
+  // inherit the font size from the parent container, such as the body.
+  fontSize: "16px",
+
+  // Override the base line height for every component.
+  lineHeight: 1.5,
+
+  // Match your application's color scheme
+  colors: {
+    brand: "#6b88bd",
+    "text-primary": "#FFFFFF",
+    "text-secondary": "#B0BEC5",
+    "text-tertiary": "#78909C",
+    background: "#181A1B",
+    "background-hover": "#23272B",
+    border: "#263238",
+    filter: "#939393",
+    summarize: "#5e749c",
+    shadow: "rgba(0,0,0,0.6)",
+  },
+
+  components: {
+    question: {
+      backgroundColor: "#23272B",
+      toolbar: {
+        backgroundColor: "#23272B",
+      }
+    },
+    table: {
+      cell: {
+        textColor: "#FFFFFF",
+        backgroundColor: "#181A1B",
+      },
+    },
+  },
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -53,7 +98,7 @@ export default function RootLayout({
           <Link href="/static-dashboard">Static Dashboard</Link>
           <Link href="/interactive-dashboard">Interactive Dashboard</Link>
         </nav>
-        <MetabaseProvider authConfig={authConfig}>{children}</MetabaseProvider>
+        <MetabaseProvider authConfig={authConfig} theme={theme}>{children}</MetabaseProvider>
       </body>
     </html>
   );
--- next-sample-pages-router/src/pages/_app.tsx
+++ next-sample-pages-router/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import {
   defineMetabaseAuthConfig,
   MetabaseProvider,
+  defineMetabaseTheme,
 } from "@metabase/embedding-sdk-react/nextjs";
 import Link from "next/link";
 import type { AppProps } from "next/app";
@@ -13,6 +14,50 @@ if (!process.env.NEXT_PUBLIC_METABASE_INSTANCE_URL) {
 const authConfig = defineMetabaseAuthConfig({
   metabaseInstanceUrl: process.env.NEXT_PUBLIC_METABASE_INSTANCE_URL,
   authProviderUri: `/api/metabase/auth`,
+});
+
+const theme = defineMetabaseTheme({
+  // Specify a font to use from the set of fonts supported by Metabase.
+  // You can set the font to "Custom" to use the custom font
+  // configured in your Metabase instance.
+  fontFamily: "Lato",
+
+  // Override the base font size for every component.
+  // This does not usually need to be set, as the components
+  // inherit the font size from the parent container, such as the body.
+  fontSize: "16px",
+
+  // Override the base line height for every component.
+  lineHeight: 1.5,
+
+  // Match your application's color scheme
+  colors: {
+    brand: "#6b88bd",
+    "text-primary": "#FFFFFF",
+    "text-secondary": "#B0BEC5",
+    "text-tertiary": "#78909C",
+    background: "#181A1B",
+    "background-hover": "#23272B",
+    border: "#263238",
+    filter: "#939393",
+    summarize: "#5e749c",
+    shadow: "rgba(0,0,0,0.6)",
+  },
+
+  components: {
+    question: {
+      backgroundColor: "#23272B",
+      toolbar: {
+        backgroundColor: "#23272B",
+      }
+    },
+    table: {
+      cell: {
+        textColor: "#FFFFFF",
+        backgroundColor: "#181A1B",
+      },
+    },
+  },
 });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -27,7 +72,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Link href="/static-dashboard">Static Dashboard</Link>
         <Link href="/interactive-dashboard">Interactive Dashboard</Link>
       </nav>
-      <MetabaseProvider authConfig={authConfig}>
+      <MetabaseProvider authConfig={authConfig} theme={theme}>
         <Component {...pageProps} />
       </MetabaseProvider>
     </div>
