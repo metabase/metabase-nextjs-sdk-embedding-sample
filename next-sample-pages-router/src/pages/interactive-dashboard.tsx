779cc1c24cd1b2d3cbe4f0b04eff752d2c011e0e
Timofei Kachalov <timofei@metabase.com>
2025-05-22 21:25:06 +0400

    Add e2e to check a dashboard as PDF download

--- next-sample-pages-router/src/pages/interactive-dashboard.tsx
+++ next-sample-pages-router/src/pages/interactive-dashboard.tsx
@@ -4,7 +4,7 @@ export default function InteractiveDashboardPage() {
   return (
     <main className="p-4">
       <h1 className="text-2xl mb-4">Interactive Dashboard Example</h1>
-      <InteractiveDashboard dashboardId={1} />
+      <InteractiveDashboard dashboardId={1} withDownloads />
     </main>
   );
 }
