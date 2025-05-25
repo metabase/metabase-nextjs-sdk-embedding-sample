433d2e64fce13e45edaa429340648c73afb69e31
Timofey Kachalov <timofei@metabase.com>
2025-05-25 21:09:33 +0400

    Add locale tests (#16)

--- e2e/support/cypress.js
+++ e2e/support/cypress.js
@@ -1,1 +1,3 @@
 import "@testing-library/cypress/add-commands";
+
+Cypress.on("uncaught:exception", (err, runnable) => false);
---
+++ next-sample-pages-router/src/components/app-provider.tsx
@@ -0,0 +1,72 @@
+import { useSearchParams } from 'next/navigation'
+import {
+  MetabaseProvider,
+  defineMetabaseAuthConfig,
+  defineMetabaseTheme
+} from '@metabase/embedding-sdk-react/nextjs';
+import { PropsWithChildren } from 'react';
+
+if (!process.env.NEXT_PUBLIC_METABASE_INSTANCE_URL) {
+  throw new Error("Missing NEXT_PUBLIC_METABASE_INSTANCE_URL");
+}
+
+const authConfig = defineMetabaseAuthConfig({
+  metabaseInstanceUrl: process.env.NEXT_PUBLIC_METABASE_INSTANCE_URL,
+  authProviderUri: `/api/metabase/auth`,
+});
+
+const theme = defineMetabaseTheme({
+  // Specify a font to use from the set of fonts supported by Metabase.
+  // You can set the font to "Custom" to use the custom font
+  // configured in your Metabase instance.
+  fontFamily: "Lato",
+
+  // Override the base font size for every component.
+  // This does not usually need to be set, as the components
+  // inherit the font size from the parent container, such as the body.
+  fontSize: "16px",
+
+  // Override the base line height for every component.
+  lineHeight: 1.5,
+
+  // Match your application's color scheme
+  colors: {
+    brand: "#6b88bd",
+    "text-primary": "#FFFFFF",
+    "text-secondary": "#B0BEC5",
+    "text-tertiary": "#78909C",
+    background: "#181A1B",
+    "background-hover": "#23272B",
+    border: "#263238",
+    filter: "#939393",
+    summarize: "#5e749c",
+    shadow: "rgba(0,0,0,0.6)",
+  },
+
+  components: {
+    question: {
+      backgroundColor: "#23272B",
+    },
+    table: {
+      cell: {
+        textColor: "#FFFFFF",
+        backgroundColor: "#181A1B",
+      },
+    },
+  },
+});
+
+export const AppProvider = ({children}: PropsWithChildren) => {
+  const searchParams = useSearchParams()
+  const locale = searchParams.get('locale') ?? undefined
+
+  return (
+    <MetabaseProvider
+      authConfig={authConfig}
+      theme={theme}
+      locale={locale}
+    >
+      {children}
+    </MetabaseProvider>
+  )
+}
