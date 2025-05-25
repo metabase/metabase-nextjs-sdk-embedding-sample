3897447268d7e9856b0a8d06c3526e2aee2bac1f
Timofei Kachalov <timofei@metabase.com>
2025-05-25 19:14:02 +0400

    Add locale tests

--- e2e/test/compatibility.cy.spec.js
+++ e2e/test/compatibility.cy.spec.js
@@ -69,7 +69,7 @@ const TIMEOUT_MS = 40000;
       });
     });
 
-    it.only("should download an Interactive Dashboard", () => {
+    it("should download an Interactive Dashboard", () => {
       cy.visit({
         url: "/interactive-dashboard",
       });
@@ -77,11 +77,57 @@ const TIMEOUT_MS = 40000;
       expect(cy.findByTestId("embed-frame", {timeout: TIMEOUT_MS}).should("exist"));
       cy.findByTestId("embed-frame", {timeout: TIMEOUT_MS}).within(() => {
         cy.findByTestId("fixed-width-dashboard-header").within(() => {
+          // Different icons for 54 and 55
           cy.get('button svg.Icon-download, button svg.Icon-document').first().click();
         });
 
         cy.readFile('cypress/downloads/E-commerce Insights.pdf', {timeout: TIMEOUT_MS}).should('exist');
       });
+    });
+
+    it("should load a metabase locale", () => {
+      cy.visit({
+        url: "/interactive-question?locale=es",
+      });
+
+      expect(cy.findByText('Tabla', {timeout: TIMEOUT_MS}).should("exist"));
+    });
+
+    it("should load a moment locale", () => {
+      cy.visit({
+        url: "/interactive-question?locale=es",
+      });
+
+      cy.findByText('Filtro', {timeout: TIMEOUT_MS}).click();
+      cy.get('[data-element-id="mantine-popover"]').within(() => {
+        cy.findByText('Created At').click();
+        cy.findByText(/(Fechas específicas…|Rango de fechas fijo…)/).click();
+      })
+
+      cy.findByTestId('date-filter-picker').within(() => {
+        const monthName = new Intl.DateTimeFormat('es-ES', { month: 'short' }).format(new Date());
+
+        cy.findByText(new RegExp(monthName, 'i')).should('exist');
+      })
+    });
+
+    it("should load a dayjs locale", () => {
+      cy.visit({
+        url: "/interactive-question?locale=es",
+      });
+
+      cy.findByText('Filtro', {timeout: TIMEOUT_MS}).click();
+      cy.get('[data-element-id="mantine-popover"]').within(() => {
+        cy.findByText('Created At').click();
+        // Different texts for 54 and 55
+        cy.findByText(/(Fechas específicas…|Rango de fechas fijo…)/).click();
+      })
+
+      cy.findByTestId('date-filter-picker').within(() => {
+        const monthName = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(new Date());
+
+        cy.findByText(new RegExp(monthName, 'i')).should('exist');
+      })
     });
   });
 });
