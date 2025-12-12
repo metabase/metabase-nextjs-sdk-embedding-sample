b633d46409bdfc8b785722cf42c8becade5a2984
Timofei Kachalov <timofei@metabase.com>
2025-12-12 21:57:18 +0400

    Override browser version for Cypress from env var

--- e2e/support/cypress.config.js
+++ e2e/support/cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     baseUrl: null,
     supportFile: path.resolve(path.join(__dirname, './cypress.js')),
     specPattern: path.resolve(path.join(__dirname, '../test/**/*.cy.spec.js')),
-    defaultBrowser: 'chrome',
+    defaultBrowser: process.env.CYPRESS_BROWSER ?? "chrome",
     env: {
       BASE_URL_APP_ROUTER: `http://localhost:${process.env.CLIENT_PORT_APP_ROUTER}`,
       BASE_URL_PAGES_ROUTER: `http://localhost:${process.env.CLIENT_PORT_PAGES_ROUTER}`,
